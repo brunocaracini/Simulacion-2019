c6c5155a7e6b9568d75363a70be3905cb67a2e69
Joaquin Cardona <joaquincardonaruiz@gmail.com>
2019-08-23 18:22:01 -0300

    Agregados comentariso a mmc

--- MMC.py
+++ MMC.py
@@ -37,22 +37,38 @@ class Linea():
 
 class Simulacion():
     def __init__(self):
+        """
+        Establece la topologia de la simulacion
+        Inicializa la simulacion en t = 0
+        Establece la lista de eventos y sus tiempos iniciales 
+        """
         self.reloj = 0
+        #creamos una lista de nombres para llamar al evento correspondiente de lista_eventos
         self.nombres_eventos = ["Arribo", "Partida11", "Partida12", "Partida13", "Partida21", "Partida22", "Partida23"]
+        
+        #creamos la topologia de la simulacion, estableciendo las lineas, servidores, y colas
         l1 = Linea(Cola_Inicial(0,0.8), [Servidor(0.5),Servidor(0.5),Servidor(0.5)])
         l2 = Linea([Cola(0),Cola(0),Cola(0)], [Servidor(0.2),Servidor(0.4),Servidor(0.6)])
         self.lineas = [l1,l2]
 
+        #Creamos la lista de eventos, estableciendo todas las partidas en big_number para evitar partidas de servidores desocupados
         self.lista_eventos = []
         self.lista_eventos.append(np.random.exponential(1/self.lineas[0].colas_entrada.tma))
-        for i in range(6):
+        for i in self.nombres_eventos[1:]:
             self.lista_eventos.append(big_number)
 
 
     def programa_principal(self):
+        """
+        Llama a las subrutinas de la simulacion
+        E itera hasta que se cumplen las condiciones de finalizacion
+        """
         pass
 
     def tiempos(self):
+        """
+        Avanza el reloj hasta el siguiente evento y pasa el control a la subrutina de evento
+        """
         nro_evento = self.lista_eventos.index(min(self.lista_eventos))
         self.reloj = self.lista_eventos[nro_evento]
         self.eventos(self.nombres_eventos[nro_evento])
@@ -60,6 +76,10 @@ class Simulacion():
 
 
     def evitar_partidas_vacias(self):
+        """
+        Asegura que no haya partidas de servidores desocupados
+        comprobando la disponibilidad de cada servidor en cada iteracion
+        """
         #si la cola esta vacio seteamos el tiempo de partida de ese servidor
         #a big_number para evitar partidas
         #si una cola esta vacia, se setean todas las partidas de los servidores vacios
@@ -67,6 +87,10 @@ class Simulacion():
         pass
 
     def eventos(self,evento):
+        """
+        Se llama con el argumento del evento correspondiente
+        Actualiza el estado del sistema de acuerdo al evento
+        """
         if evento == "Arribo":
             cli = Cliente()
             nro_servidor = self.asignar_servidor()
@@ -124,11 +148,18 @@ class Simulacion():
             self.lineas[1].servidores[2].salida_servidor()
             
     def asignar_servidor(self):
+        """
+        Establece una lista de prioridades para comprobar la disponibilidad de los servidores
+        de la primer linea y asigna el servidor correspondiente
+        """
         #Si todos los servidores estan ocupados, devuelve False
         #Sino, devuelve nro de servidor
         return False
 
     def reportes(self):
+        """
+        Genera los reportes estadisticos al finalizar la simulacion
+        """
         pass
 
 
