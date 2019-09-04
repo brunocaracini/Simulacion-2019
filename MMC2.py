5bae9cb87a888ab03e0413775de65d5a7c2f86e4
brunocaracini <39558094+brunocaracini@users.noreply.github.com>
2019-09-04 00:43:03 -0300

    Add files via upload

--- MMC2.py
+++ MMC2.py
@@ -50,24 +50,26 @@ class Simulacion():
         self.colas = [self.cola0,self.cola1,self.cola2,self.cola3]
         self.linea1 = [self.serv1, self.serv2, self.serv3]
         self.linea2 = [self.serv4, self.serv5, self.serv6]
-        self.clientes_completaron_demora = 0
+        self.demora_acumulada = 0
+        self.clientes_completaron_demora = 0 
 
     def inicializacion(self):
         self.clock = 0  #Inicializamos el reloj en 0.
         tiempo = self.clock + np.random.exponential(1/tma)  #Calculamos el tiempo del primer arribo para inicializar la lista de eventos.
         prox_evento = Evento('arribo', tiempo) #Creamos un objeto del tipo evento, que recibe como parametro el tiempo calculado y el tipo de evento.
-        self.l_evento[0] = prox_evento  #Asignamos el evento arribo que creamos en la posición 0 de la lista de eventos.
+        self.l_evento.append(prox_evento)  #Asignamos el evento arribo que creamos en la posición 0 de la lista de eventos.
         for i in range(1,7): #Llenamos las posiciones restantes de la lista de eventos con partidas.
             tiempo = infinito #Ponemos el tiempo en infinito para estas partidas.
             prox_evento = Evento('partida', tiempo) #Creamos un objeto del tipo evento, que recibe como parametro el tiempo calculado y el tipo de evento.
-            self.l_evento[i] = prox_evento # Asignamos el evento arribo que creamos en la posición i de la lista de eventos.
+            self.l_evento.append(prox_evento) # Asignamos el evento arribo que creamos en la posición i de la lista de eventos.
 
 
     def tiempos(self):
         i = 0
+        self.l_tiempo = []
         for evento in self.l_evento: #En este for se obtiene el tiempo de todos los eventos de la lista de eventos, y lo guardamos en una lista de tiempos.
             i +=1
-            self.l_tiempo[i] = evento.tiempo
+            self.l_tiempo.append(evento.tiempo)
         tiempo_prox_evento = min(self.l_tiempo) #Elegimos el mas chico de esa lista de tiempos.
         index_prox_evento = self.l_tiempo.index(tiempo_prox_evento) 
         evento = self.l_evento[index_prox_evento] #Buscamos el evento que corresponde a ese tiempo en la lista de eventos
@@ -76,11 +78,6 @@ class Simulacion():
         
 
     def eventos(self, evento_tipo, index_prox_evento):
-        if evento_tipo == 'arribo':
-            arribo()
-        if evento_tipo == 'partida':
-            partida(index_prox_evento)
-
         def arribo():
             tiempo = self.clock + np.random.exponential(1/tma) #Calcula el nuevo tiempo de arribo.
             prox_evento = Evento('arribo', tiempo) #Creamos un objeto del tipo Evento, que recibe como parametro el tiempo calculado y el tipo de evento.
@@ -110,20 +107,20 @@ class Simulacion():
                     self.colas[index_prox_evento].clientes.append(cliente)
                 else:
                     self.linea2[index_prox_evento - 1].cliente = cliente
-                    tiempo = self.clock + np.random.exponential(1/servidor.tms)
+                    tiempo = self.clock + np.random.exponential(self.linea1[index_prox_evento - 1].tms)
                     prox_partida = Evento('partida', tiempo)
                     self.l_evento[index_prox_evento] = prox_partida
                     
                 self.linea1[index_prox_evento - 1].cliente = False   
                 self.linea1[index_prox_evento - 1].ocupado == False
 
-                if len(self.colas[0]) == 0:
+                if len(self.colas[0].clientes) == 0:
                     prox_partida = Evento('partida', infinito)
                     self.l_evento[index_prox_evento] = prox_partida
                 else:
                     cliente = self.colas[0].clientes.pop()
                     self.linea1[index_prox_evento - 1].cliente = cliente
-                    tiempo = self.clock + np.random.exponential(1/servidor.tms)
+                    tiempo = self.clock + np.random.exponential(self.linea1[index_prox_evento - 1].tms)
                     prox_partida = Evento('partida', tiempo)
                     self.l_evento[index_prox_evento] = prox_partida
 
@@ -134,42 +131,53 @@ class Simulacion():
                 else:
                     cliente = self.colas[index_prox_evento - 3].pop()
                     self.linea2[index_prox_evento - 4].cliente = cliente
-                    tiempo = self.clock + np.random.exponential(1/servidor.tms)
+                    tiempo = self.clock + np.random.exponential(self.linea2[index_prox_evento - 3].tms)
                     prox_partida = Evento('partida', tiempo)
                     self.l_evento[index_prox_evento] = prox_partida
 
                 self.linea2[index_prox_evento - 4].cliente = False
                 self.linea2[index_prox_evento - 4].ocupado = False
 
-                    
+
+        if evento_tipo == 'arribo':
+            arribo()
+        if evento_tipo == 'partida':
+            partida(index_prox_evento)
 
 
-                
-
-
-                
-                    
-                
     def reportes(self):
-        pass    
+        #Utilizacion de servidores u(t)
+        u_t = []
+        for servidor in (self.linea1+self.linea2):
+            u_t.append(servidor.tiempo_acumulado_servicio/self.clock)
+
+        #Demora promedio del cliente
+        d_t_promedio =  self.demora_acumulada/self.clientes_completaron_demora
+
+        #Cantidad promedio de clientes en cola q_t
+        q_t = []
+        for cola in self.colas:
+            q_t.append(cola.q_t / self.clock)
             
+        print("Utilizaciones de servidores: ",u_t)
+        print("Demora promedio de clientes: ",d_t_promedio)
+        print("Cantidad promedio de clientes en cola: ",q_t)
+        
+
     def programa_principal(self):
         #LLamamos a la rutina de inicialización.
         self.inicializacion()
 
-        #Llamamos a la rutina de tiempos, que devuelve el proximo evento, y lo guardamos en una variable.
-        array = self.tiempos()
-        tipo_prox_evento = array[0]
-        index_prox_evento = array[1]
-        
-        #LLamamos a la rutina de eventos, y le mandamos como parámetro el tipo de evento del proximo evento.
-        self.eventos(tipo_prox_evento, index_prox_evento)
+        while(self.clock < 50):
+            #Llamamos a la rutina de tiempos, que devuelve el proximo evento, y lo guardamos en una variable.
+            print(self.clock)
+            array = self.tiempos()
+            tipo_prox_evento = array[0]
+            index_prox_evento = array[1]
+            
+            #LLamamos a la rutina de eventos, y le mandamos como parámetro el tipo de evento del proximo evento.
+            self.eventos(tipo_prox_evento, index_prox_evento)
 
 
-
-        
-              
-
-
-
-
+sim = Simulacion()
+sim.programa_principal()
