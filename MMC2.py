217b881f47be5c291e7700405b98129bcad2931d
brunocaracini <39558094+brunocaracini@users.noreply.github.com>
2019-09-04 20:59:30 -0300

    Add files via upload

--- MMC2.py
+++ MMC2.py
@@ -5,9 +5,15 @@ infinito = 99999999999
 tma = 0.8
 
 class Cliente():
-    def __init__(self, tiempo_entrada, tiempo_salida):
-        self.tiempo_entrada = tiempo_entrada
-        self.tiempo_salida = tiempo_salida
+    def __init__(self):
+        self.tiempo_entrada = 0
+        self.tiempo_salida = 0
+    
+    def entrada(self, tiempo):
+        self.tiempo_entrada = tiempo
+
+    def salida(self, tiempo):
+        self.tiempo_salida = tiempo
 
 class Cola():
     def __init__(self):
@@ -18,15 +24,32 @@ class Servidor():
         self.ocupado = estado
         self.cliente = False
         self.numero = numero
-        self.tms = tiempo_m_servicio
-        #En la funcion de ingreso al servidor (que vas a hacer porque sos buen programador)
-        #Seteas el tiempo de entrada del cliente actual al reloj en el momento
-
-        #Y en la funcion de salida del servidor (que tambien vas a hacer)
-        #Le sumas al tiempo acumulado de servicio (clock - tiempo_entrada_cliente_actual)
+        self.entrada_serv = 0
+        self.salida_serv = 0
         self.tiempo_acumulado_servicio = 0
-        self.tiempo_entrada_cliente_actual = 0
-    
+        self.tms = tiempo_m_servicio
+        
+    def asigna_cliente(self, cliente, tiempo):
+        if cliente == False:
+            self.salida_servidor(tiempo)
+            self.ocupado = False
+            self.tiempo_acumlado()
+        else:
+            self.cliente = cliente
+            self.entrada_servidor(tiempo)
+            self.ocupado = True
+
+    def entrada_servidor(self, tiempo):
+        self.entrada_serv = tiempo
+
+    def salida_servidor(self, tiempo):
+        self.salida_serv = tiempo
+        self.cliente = False
+
+    def tiempo_acumlado(self):
+        self.tiempo_acumulado_servicio = self.tiempo_acumulado_servicio + (self.salida_serv - self.entrada_serv)
+
+
 class Evento():
     def __init__(self,tipo,tiempo):
         self.tipo = tipo
@@ -96,7 +119,8 @@ class Simulacion():
             #Creamos un objeto del tipo Evento, que recibe como parametro el tiempo calculado y el tipo de evento.
             self.l_evento[0] = prox_evento 
             #Asignamos el evento arribo que creamos en la posición 0 de la lista de eventos.
-            cliente = Cliente(self.clock, 0) 
+            cliente = Cliente()
+            cliente.entrada(self.clock)
             #Creamos un objeto del tipo Cliente.
             servidores_disponibles = [] 
             #Creamos un arreglo vacío de servidores disponibles.
@@ -112,9 +136,8 @@ class Simulacion():
             #Si hubo servidores disponibles, entonces:
                 servidor = random.choice(servidores_disponibles) 
                 #Elegimos un servidor al azar de la lista de servidores disponibles.
-                servidor.cliente = cliente  
+                servidor.asigna_cliente(cliente, self.clock)  
                 #Registramos que el elciente se encuentra en ese servidor
-                servidor.ocupado = True 
                 #Actualizamos el estado de ocupación del servidor.
                 index_serv = self.linea1.index(servidor)    
                 self.linea1[index_serv] = servidor 
@@ -133,58 +156,59 @@ class Simulacion():
                     self.colas[index_prox_evento].clientes.append(cliente)
                 #Chequeamos si el servidor que está en línea recta está ocupado, y si lo está asignamos el cliente a la cola.
                 else:
-                    self.linea2[index_prox_evento - 1].cliente = cliente
+                    self.linea2[index_prox_evento - 1].asigna_cliente(cliente, self.clock)
                     #En caso de que esté vacío, asignamos al cliente a ese servidor.
+                    #Ponemos el servidor al que paso el cliente como ocupado.
                     tiempo = self.clock + np.random.exponential(self.linea1[index_prox_evento - 1].tms)
                     prox_partida = Evento('partida', tiempo)
                     self.l_evento[index_prox_evento + 3] = prox_partida
                     #Calculamos un evento partida para ese cliente en el nuevo servidor.
-                    self.linea2[index_prox_evento - 1].ocupado = True
-                    #Ponemos el servidor al que paso el cliente como ocupado.
+                    
 
-                self.linea1[index_prox_evento - 1].cliente = False   
-                self.linea1[index_prox_evento - 1].ocupado == False
+                self.linea1[index_prox_evento - 1].asigna_cliente(False, self.clock)   
                 #Cambiamos el estado del servidor y borramos el cliente que ha partido.
 
                 if len(self.colas[0].clientes) == 0:
                     prox_partida = Evento('partida', infinito)
                     self.l_evento[index_prox_evento] = prox_partida
                 # Chequeamos si la longitud de la cola que alimenta la primera linea es 0, si es así, 
-                # ponemos la partida de ese srvidor en infinito
+                # ponemos la partida de ese servidor en infinito
                 else:
                 #Si la cola no está vacia, entonces:
                     cliente = self.colas[0].clientes.pop()
                     #agarramos el primer cliente de la cola y lo guardamos en una variable, moviendo
                     #los demás clientes una posición hacia adelante.
-                    self.linea1[index_prox_evento - 1].cliente = cliente
+                    self.linea1[index_prox_evento - 1].asigna_cliente(cliente, self.clock)
                     #asignamos el cliente al servidor que quedó vacío en la partida.
+                    #Cambiamos el estado del servidor a ocupado.
                     tiempo = self.clock + np.random.exponential(self.linea1[index_prox_evento - 1].tms)
                     prox_partida = Evento('partida', tiempo)
                     self.l_evento[index_prox_evento] = prox_partida
                     #Calculamos un evento partida para ese cliente, y lo guardamos en la lista de eventos.
-                    self.linea1[index_prox_evento -1].ocupado = True
-                    #Cambiamos el estado del servidor a ocupado.
-            
+                    
             else:
             #Si la partida no fue de la primera línea, entonces:
                 
-                if len(self.colas[index_prox_evento - 3]) == 0:
+                self.linea2[index_prox_evento - 4].cliente.salida(self.clock)
+                #Registramos el tiempo en el que el cliente sale del sistema
+
+                self.linea2[index_prox_evento - 4].asigna_cliente(False, self.clock)
+                #Cambiamos el estado del servidor a disponible
+
+                if len(self.colas[index_prox_evento - 3].clientes) == 0:
                 #Chequeamos si la cola que alimenta el servidor está vacía.    
                     prox_partida = Evento('partida', infinito)
                     self.l_evento[index_prox_evento] = prox_partida
                     #Si lo está, ponemos la partida de ese servidor en infinito, y actualizamos la lista de eventos.
-                    self.linea2[index_prox_evento - 4].ocupado = False
-                    self.linea2[index_prox_evento - 4].cliente = False
-                    #Cambiamos el estado del servidor a disponible
-                
+                    
                 else:
                 #Si la cola no estaba vacía, entonces:
-                    cliente = self.colas[index_prox_evento - 3].pop()
+                    cliente = self.colas[index_prox_evento - 3].clientes.pop()
                     #agarramos el primer cliente de la cola y lo guardamos en una variable, moviendo
-                    #los demás clientes una posición hacia adelante.                    
-                    self.linea2[index_prox_evento - 4].cliente = cliente
+                    #los demás clientes una posición hacia adelante.                
+                    self.linea2[index_prox_evento - 4].asigna_cliente(cliente, self.clock)
                     #asignamos el cliente al servidor que quedó vacío en la partida.
-                    tiempo = self.clock + np.random.exponential(self.linea2[index_prox_evento - 3].tms)
+                    tiempo = self.clock + np.random.exponential(self.linea2[index_prox_evento - 4].tms)
                     prox_partida = Evento('partida', tiempo)
                     self.l_evento[index_prox_evento] = prox_partida
                     #Calculamos un evento partida para el cliente que se asigna al servidor.
@@ -195,13 +219,13 @@ class Simulacion():
         if evento_tipo == 'partida':
             partida(index_prox_evento)
 
-
+    
     def reportes(self):
         #Utilizacion de servidores u(t)
         u_t = []
         for servidor in (self.linea1+self.linea2):
             u_t.append(servidor.tiempo_acumulado_servicio/self.clock)
-
+        '''
         #Demora promedio del cliente
         d_t_promedio =  self.demora_acumulada/self.clientes_completaron_demora
 
@@ -209,17 +233,17 @@ class Simulacion():
         q_t = []
         for cola in self.colas:
             q_t.append(cola.q_t / self.clock)
-            
+        ''' 
         print("Utilizaciones de servidores: ",u_t)
-        print("Demora promedio de clientes: ",d_t_promedio)
-        print("Cantidad promedio de clientes en cola: ",q_t)
+        #print("Demora promedio de clientes: ",d_t_promedio)
+        #print("Cantidad promedio de clientes en cola: ",q_t)
         
 
     def programa_principal(self):
         #LLamamos a la rutina de inicialización.
         self.inicializacion()
 
-        while(self.clock < 50):
+        while(self.clock < 5000):
             #Llamamos a la rutina de tiempos, que devuelve el proximo evento, y lo guardamos en una variable.
             print(self.clock)
             array = self.tiempos()
@@ -229,6 +253,7 @@ class Simulacion():
             #LLamamos a la rutina de eventos, y le mandamos como parámetro el tipo de evento del proximo evento.
             self.eventos(tipo_prox_evento, index_prox_evento)
 
+        self.reportes()
 
 sim = Simulacion()
 sim.programa_principal()
